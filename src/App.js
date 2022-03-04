f61ba68136f7382f4f74317af45f93c96f1a55b7
com194 <umang.mittal@daffodilsw.com>
2022-03-04 15:47:00 +0530

    Added Navbar and footer components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import Footer from "./components/Footer";
 import Home from "./components/home";
+import Login from "./components/login";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <Home></Home>
+      <Footer />
+      {/* <Login /> */}
     </>
   );
 }
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -3,144 +3,24 @@ import React from "react";
 export default function Home() {
   return (
     <>
-      <div className="navbar navbar-inverse navbar-fixed-top">
-        <div className="navbar-inner">
-          <div className="container">
-            <button
-              type="button"
-              className="btn btn-navbar"
-              data-toggle="collapse"
-              data-target=".nav-collapse"
-            >
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-              <span className="icon-bar"></span>
-            </button>
-            <a className="brand" href="">
-              PPL
-            </a>
-            <div className="pro_info pull-right">
-              <div className="pro_icn">
-                <img src="./assets/images/pic_small.png" />
-              </div>
-              <div className="pro_txt">
-                Me<b className="caret"></b>
-              </div>
-              <ul
-                className="dropdown-menu"
-                role="menu"
-                aria-labelledby="dLabel"
-              >
-                <li>
-                  <a tabIndex="-1" href="#">
-                    My Profile
-                  </a>
-                </li>
-                <li>
-                  <a tabIndex="-1" href="#">
-                    Message Box
-                  </a>
-                </li>
-                <li>
-                  <a tabIndex="-1" href="#">
-                    Change Language
-                  </a>
-                </li>
-                <li className="divider"></li>
-                <li>
-                  <a tabIndex="-1" href="#">
-                    <input type="text" placeholder="search" />
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="nav-collapse collapse">
-              <ul className="nav">
-                <li className="active">
-                  <a href="">Home</a>
-                </li>
-                <li className="">
-                  <a href="">E-Coupons</a>
-                </li>
-                <li className="">
-                  <a href="">E-Brands</a>
-                </li>
-                <li className="">
-                  <a href="">Resuse Market</a>
-                </li>
-                <li className="">
-                  <a href="">Lost and Found</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="header">
-        <div className="header_lft">
-          <div className="logo">
-            <a href="#">
-              <img src="images/logo.png" />
-            </a>
-          </div>
-          <div className="navigatn">
-            <ul>
-              <li>
-                <a href="#" className="active">
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#"> E-Coupons </a>
-              </li>
-              <li>
-                <a href="#">E-Brands </a>
-              </li>
-              <li>
-                <a href="#"> Resuse Market </a>
-              </li>
-              <li>
-                <a href="#"> Lost and Found</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="header_rgt">
-          <div className="flag_div">
-            <img src="images/flag.png" />
-          </div>
-          <input type="text" placeholder="Search" className="txt_box" />
-          <div className="msg_box">
-            <a href="#">
-              <span className="msg_count">100</span>
-            </a>
-          </div>
-          <div className="info_div">
-            <div className="image_div">
-              <img src="images/pic.png" />
-            </div>
-            <div className="info_div1">Me</div>
-          </div>
-        </div>
-      </div>
       <div className="container">
         <div className="content">
           <div className="content_rgt">
             <div className="rght_btn">
               <span className="rght_btn_icon">
-                <img src="images/btn_iconb.png" alt="up" />
+                <img src={require("../assets/images/btn_iconb.png")} alt="up" />
               </span>
               <span className="btn_sep">
-                <img src="images/btn_sep.png" alt="sep" />
+                <img src={require("../assets/images/btn_sep.png")} alt="sep" />
               </span>
               <a href="#">Upload Post</a>
             </div>
             <div className="rght_btn">
               <span className="rght_btn_icon">
-                <img src="images/btn_icona.png" alt="up" />
+                <img src={require("../assets/images/btn_icona.png")} alt="up" />
               </span>
               <span className="btn_sep">
-                <img src="images/btn_sep.png" alt="sep" />
+                <img src={require("../assets/images/btn_sep.png")} alt="sep" />
               </span>
               <a href="#">Invite Friends</a>
             </div>
@@ -153,7 +33,10 @@ export default function Home() {
                   <li>
                     <a href="#">
                       <span className="list_icon">
-                        <img src="images/icon_01.png" alt="up" />
+                        <img
+                          src={require("../assets/images/icon_01.png")}
+                          alt="up"
+                        />
                       </span>
                       CATS
                     </a>
@@ -161,7 +44,10 @@ export default function Home() {
                   <li>
                     <a href="#">
                       <span className="list_icon">
-                        <img src="images/icon_02.png" alt="up" />
+                        <img
+                          src={require("../assets/images/icon_02.png")}
+                          alt="up"
+                        />
                       </span>
                       Dogs
                     </a>
@@ -169,7 +55,10 @@ export default function Home() {
                   <li>
                     <a href="#">
                       <span className="list_icon">
-                        <img src="images/icon_03.png" alt="up" />
+                        <img
+                          src={require("../assets/images/icon_03.png")}
+                          alt="up"
+                        />
                       </span>
                       Birds
                     </a>
@@ -177,7 +66,10 @@ export default function Home() {
                   <li>
                     <a href="#">
                       <span className="list_icon">
-                        <img src="images/icon_04.png" alt="up" />
+                        <img
+                          src={require("../assets/images/icon_04.png")}
+                          alt="up"
+                        />
                       </span>
                       Rabbit
                     </a>
@@ -185,7 +77,10 @@ export default function Home() {
                   <li>
                     <a href="#">
                       <span className="list_icon">
-                        <img src="images/icon_05.png" alt="up" />
+                        <img
+                          src={require("../assets/images/icon_05.png")}
+                          alt="up"
+                        />
                       </span>
                       Others
                     </a>
@@ -457,40 +352,6 @@ export default function Home() {
           </div>
         </div>
         <div className="clear"></div>
-      </div>
-      <div className="footr">
-        <div className="footr_lft">
-          <div className="footer_div1">
-            Copyright © Pet-Socail 2014 All Rights Reserved
-          </div>
-          <div className="footer_div2">
-            <a href="#">Privacy Policy </a>| <a href="#"> Terms & Conditions</a>
-          </div>
-        </div>
-        <div className="footr_rgt">
-          <ul>
-            <li>
-              <a href="#">
-                <img src="images/social_1.png" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="images/social_2.png" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="images/social_3.png" />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <img src="images/social_4.png" />
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
     </>
   );
