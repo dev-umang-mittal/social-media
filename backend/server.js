675c4a75d27d61e1e91942ecb5f4e2c7ef608fe0
com194 <umang.mittal@daffodilsw.com>
2022-03-07 16:03:01 +0530

    Added Error Handler

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const mongoose = require("mongoose");
 const app = express();
 // app.use(express.urlencoded({extended:true}));
 // app.use(express.json());
+const errorHandler = require("./errorHandler");
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 
@@ -30,3 +31,9 @@ app.use("/post", postRouter);
 // => Comment Routes
 const commentRoutes = require("./routes/commentRoutes");
 app.use("/comment", commentRoutes);
+
+app.use(errorHandler);
+
+app.listen(8080, () => {
+  console.info("Server is listening on port 8080");
+});
