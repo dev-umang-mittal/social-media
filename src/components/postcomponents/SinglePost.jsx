ae2ce6f612117003f6e02554895744810e61215f
com194 <umang.mittal@daffodilsw.com>
2022-03-15 18:20:59 +0530

    Connected single post component with backend

--- src/components/postcomponents/SinglePost.jsx
+++ src/components/postcomponents/SinglePost.jsx
@@ -1,25 +1,67 @@
-import React from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import { useAlert } from "react-alert";
 import { useParams } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function SinglePost() {
-  let { id } = useParams();
+  const { user } = useContext(AuthContext);
+  const { id } = useParams();
+  const [post, setPost] = useState();
+  const alert = useAlert();
+  const [comments, setComments] = useState([]);
+  const comment = useRef();
   //TODO: use the id to fetch a certain blog.
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_TESTING_URL}/post/${id}`)
+      .then((res) => {
+        setPost(res.data);
+        getComments(res.data._id);
+      })
+      .catch((e) => {
+        alert.error(e.response.statusText);
+      });
+  }, []);
+
+  function getComments(id) {
+    axios
+      .get(`${process.env.REACT_APP_TESTING_URL}/comment/blog/${id}`)
+      .then((res) => {
+        setComments(res.data);
+      })
+      .catch((e) => {
+        alert.error(e.response.statusText);
+      });
+  }
+
+  function submitComment(id) {
+    axios.post(`${process.env.REACT_APP_TESTING_URL}/comment/create`, {
+      commentedOnId: id,
+      comment: comment.current.value,
+      commenter: {
+        name: user.response.name,
+        image: user.response.image,
+        id: user.response.id,
+        username: user.response.username,
+      },
+    });
+  }
 
   return (
     <>
       <div className="content_lft">
         <div className="contnt_2">
           <div className="div_a">
-            <div className="div_title">
-              User Interface PSD Source files Web Designing for web
-            </div>
+            <div className="div_title">{post && post.title}</div>
             <div className="btm_rgt">
               <div className="btm_arc">Cats</div>
             </div>
             <div className="div_top">
               <div className="div_top_lft">
-                <img src="images/img_6.png" />
-                Steave Waugh
+                {post && <img src={post.authorDetails.image} />}
+                {post && post.authorDetails.name}
               </div>
               <div className="div_top_rgt">
                 <span className="span_date">02 Jan 2014</span>
@@ -27,41 +69,53 @@ export default function SinglePost() {
               </div>
             </div>
             <div className="div_image">
-              <img src="images/lft_img.png" alt="pet" />
+              {post && <img src={post.image} alt="pet" />}
             </div>
             <div className="div_btm">
               <div className="btm_list">
                 <ul>
                   <li>
-                    <a href="#">
+                    <a>
                       <span className="btn_icon">
-                        <img src="images/icon_001.png" alt="share" />
+                        <img
+                          src={require("../../assets/images/icon_001.png")}
+                          alt="share"
+                        />
                       </span>
                       Share
                     </a>
                   </li>
                   <li>
-                    <a href="#">
+                    <a>
                       <span className="btn_icon">
-                        <img src="images/icon_002.png" alt="share" />
+                        <img
+                          src={require("../../assets/images/icon_002.png")}
+                          alt="share"
+                        />
                       </span>
                       Flag
                     </a>
                   </li>
                   <li>
-                    <a href="#">
+                    <a>
                       <span className="btn_icon">
-                        <img src="images/icon_003.png" alt="share" />
+                        <img
+                          src={require("../../assets/images/icon_003.png")}
+                          alt="share"
+                        />
                       </span>
-                      0 Likes
+                      {post && post.likes} Likes
                     </a>
                   </li>
                   <li>
-                    <a href="#">
+                    <a>
                       <span className="btn_icon">
-                        <img src="images/icon_004.png" alt="share" />
+                        <img
+                          src={require("../../assets/images/icon_004.png")}
+                          alt="share"
+                        />
                       </span>
-                      4 Comments
+                      {post && post.comments} Comments
                     </a>
                   </li>
                 </ul>
@@ -71,71 +125,32 @@ export default function SinglePost() {
         </div>
         <div className="contnt_3">
           <ul>
-            <li>
-              <div className="list_image">
-                <div className="image_sec">
-                  <img src="images/post_img.png" />
-                </div>
-                <div className="image_name">Bharat</div>
-              </div>
-              <div className="list_info">
-                This is an example of a comment. You can create as many comments
-                like this one or sub comments as you like and manage all of your
-                content inside your Account.
-              </div>
-              <input type="button" defaultValue="Reply" className="orng_btn" />
-            </li>
-            <li>
-              <div className="list_image">
-                <div className="image_sec">
-                  <img src="images/post_img.png" />
-                </div>
-                <div className="image_name">Bharat</div>
-              </div>
-              <div className="list_info">
-                This is an example of a comment. You can create as many comments
-                like this one or sub comments as you like and manage all of your
-                content inside your Account.
-              </div>
-              <input type="button" defaultValue="Reply" className="black_btn" />
-              <div className="cmnt_div">
-                <input
-                  type="text"
-                  defaultValue="Add a Comment"
-                  className="cmnt_bx"
-                />
-                <input
-                  type="submit"
-                  className="sub_bttn"
-                  defaultValue="Submit Comment"
-                />
-              </div>
-            </li>
-            <li>
-              <div className="list_image">
-                <div className="image_sec">
-                  <img src="images/post_img.png" />
-                </div>
-                <div className="image_name">Bharat</div>
-              </div>
-              <div className="list_info">
-                This is an example of a comment. You can create as many comments
-                like this one or sub comments as you like and manage all of your
-                content inside your Account.
-              </div>
-              <input type="button" defaultValue="Reply" className="orng_btn" />
-            </li>
+            {comments.map((comment) => {
+              return (
+                <li>
+                  <div className="list_image">
+                    <div className="image_sec">
+                      <img src={comment.commenter.image} />
+                    </div>
+                    <div className="image_name">{comment.commenter.name}</div>
+                  </div>
+                  <div className="list_info">{comment.comment}</div>
+                </li>
+              );
+            })}
             <li>
               <div className="cmnt_div1">
                 <input
                   type="text"
                   defaultValue="Enter your Comment"
                   className="cmnt_bx1"
+                  ref={comment}
                 />
                 <input
                   type="submit"
                   className="sub_bttn1"
                   defaultValue="Submit Comment"
+                  onClick={() => submitComment(id)}
                 />
               </div>
             </li>
