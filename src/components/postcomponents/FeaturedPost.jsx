6086172f11605ba408ff1dbe43b468b74d633931
com194 <umang.mittal@daffodilsw.com>
2022-03-25 11:55:21 +0530

    Added link tag in featured posts

--- src/components/postcomponents/FeaturedPost.jsx
+++ src/components/postcomponents/FeaturedPost.jsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function FeaturedPost({ postDetails }) {
   return (
-    <div className="feat_sec">
-      <div className="feat_sec_img">
-        <img src={postDetails.image} alt="image" />
+    <Link to={`/post/${postDetails._id}`}>
+      <div className="feat_sec">
+        <div className="feat_sec_img">
+          <img src={postDetails.image} alt="image" />
+        </div>
+        <div className="feat_txt">{postDetails.title}</div>
+        <div className="btm_rgt">
+          <div className="btm_arc">{postDetails.tags}</div>
+        </div>
       </div>
-      <div className="feat_txt">{postDetails.title}</div>
-      <div className="btm_rgt">
-        <div className="btm_arc">{postDetails.tags}</div>
-      </div>
-    </div>
+    </Link>
   );
 }
