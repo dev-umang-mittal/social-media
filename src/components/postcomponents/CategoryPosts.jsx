54b1e95ab0e319d40ddcb3b4464813f2f6d688b8
com194 <umang.mittal@daffodilsw.com>
2022-03-29 13:18:00 +0530

    Added filtered call to category posts

--- src/components/postcomponents/CategoryPosts.jsx
+++ src/components/postcomponents/CategoryPosts.jsx
@@ -6,18 +6,18 @@ import PostComponent from "./PostComponent";
 
 export default function CategoryPosts() {
   const alert = useAlert();
-  const [posts, setPosts] = useState();
+  const [posts, setPosts] = useState([]);
   const params = useParams();
-  const { search } = useLocation();
-  const query = new URLSearchParams(search);
-  const [pageNo, setPageNo] = useState(0);
+  const [filter, setFilter] = useState({
+    tag: params.tag,
+    limit: 4,
+    skip: 0,
+  });
 
   useEffect(() => {
     axios
       .get(
-        `${process.env.REACT_APP_TESTING_URL}/post/category/${
-          params.tag
-        }?page=${query.get("page")}`
+        `${process.env.REACT_APP_TESTING_URL}/post/?tag=${filter.tag}&limit=${filter.limit}&skip=${filter.skip}`
       )
       .then((res) => {
         setPosts(res.data);
@@ -25,22 +25,25 @@ export default function CategoryPosts() {
       .catch((e) => {
         alert.error(JSON.stringify(e));
       });
-  }, [params.tag, pageNo]);
+  }, [filter]);
+
+  useEffect(() => {
+    setFilter({ ...filter, tag: params.tag });
+  }, [params.tag]);
 
   return (
     <>
       <div className="content_lft">
-        {posts && posts.length == 0 && <h3>No posts are available.</h3>}
-        {posts &&
-          posts.map((post) => {
-            return (
-              <PostComponent
-                key={post.title}
-                postDetails={post}
-                setPostDetails={setPosts}
-              ></PostComponent>
-            );
-          })}
+        {posts.length == 0 && <h3>No posts are available.</h3>}
+        {posts.map((post) => {
+          return (
+            <PostComponent
+              key={post.title}
+              postDetails={post}
+              setPostDetails={setPosts}
+            ></PostComponent>
+          );
+        })}
       </div>
       <div className="clear" />
       <div>
@@ -48,18 +51,21 @@ export default function CategoryPosts() {
           type="button"
           value="Prev"
           onClick={() => {
-            setPageNo((prev) => {
-              if (prev > 0) return prev - 1;
+            setFilter((prev) => {
+              if (prev.skip > 0)
+                return { ...prev, skip: prev.skip - prev.limit };
               else return prev;
             });
           }}
         ></input>
-        {posts && posts.length > 0 && (
+        {posts.length > 0 && (
           <input
             type="button"
             value="Next"
             onClick={() => {
-              setPageNo((prev) => prev + 1);
+              setFilter((prev) => {
+                return { ...prev, skip: prev.skip + prev.limit };
+              });
             }}
           ></input>
         )}
