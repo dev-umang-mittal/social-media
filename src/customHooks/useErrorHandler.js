789ac64dea248dea658147f63f4cbcf5b5c384e4
com194 <umang.mittal@daffodilsw.com>
2022-03-29 15:56:31 +0530

    Created a custom error handler hook

--- src/customHooks/useErrorHandler.js
+++ src/customHooks/useErrorHandler.js
@@ -5,7 +5,33 @@ export default function useErrorHandler() {
 
   const showError = (err = null) => {
     if (!err) alert.error("Somthing went wrong");
-    if (err.response) {
+    if (err.code) {
+      switch (err.code) {
+        case 1:
+          alert.error("Account Not found. Try again");
+          break;
+        case 2:
+          alert.error("You must be logged in to perform this action");
+          break;
+        case 3:
+          alert.error("Link Expired");
+          break;
+        case 4:
+          alert.error("Password's don't match");
+          break;
+        case 5:
+          alert.error("Please Login Again");
+          break;
+        case 6:
+          alert.error("Fill in all the details to continue");
+          break;
+        case 11:
+          alert.success("Success");
+          break;
+        default:
+          alert.error("Something is wrong");
+      }
+    } else if (err.response) {
       alert.error(err.response.statusText);
     } else if (err.request) {
       if (err.request.response == "") {
