6959d0ebf4bf5e0a06201493cda52591ed925b36
com194 <umang.mittal@daffodilsw.com>
2022-03-30 10:29:56 +0530

    Created Protected route

--- src/protectedRoute.js
+++ src/protectedRoute.js
@@ -1,11 +1,16 @@
 import React, { useContext } from "react";
 import { Route, Navigate } from "react-router-dom";
 import { AuthContext } from "./context/AuthContext";
+import useErrorHandler from "./customHooks/useErrorHandler";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
+  const errorHandler = useErrorHandler();
 
-  if (!user) return <Navigate to="/" />;
+  if (!user) {
+    errorHandler({ code: 2 });
+    return <Navigate to="/" />;
+  }
   return children;
 };
 
